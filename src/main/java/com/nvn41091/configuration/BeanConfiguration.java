3f238784f18bcb418477667cfbf80697e9a40578
Nguyễn Văn Ngọc <nvn41091@gmail.com>
2020-11-12 14:51:06 +0700

    cm

--- src/main/java/com/nvn41091/configuration/BeanConfiguration.java
+++ src/main/java/com/nvn41091/configuration/BeanConfiguration.java
@@ -1,12 +1,26 @@
 package com.nvn41091.configuration;
 
+import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.context.support.ResourceBundleMessageSource;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
 public class BeanConfiguration {
+
+    @Value("${cloudinary.cloud_name}")
+    private String cloudName;
+
+    @Value("${cloudinary.api_key}")
+    private String apiKey;
+
+    @Value("${cloudinary.api_secret}")
+    private String apiSecret;
 
     @Bean
     public static PropertySourcesPlaceholderConfigurer properties() {
@@ -24,5 +38,16 @@ public class BeanConfiguration {
         messageSource.setDefaultEncoding("UTF-8");
         messageSource.setUseCodeAsDefaultMessage(true);
         return messageSource;
+    }
+
+    @Bean
+    public Cloudinary cloudinaryConfig() {
+        Cloudinary cloudinary = null;
+        Map config = new HashMap();
+        config.put("cloud_name", cloudName);
+        config.put("api_key", apiKey);
+        config.put("api_secret", apiSecret);
+        cloudinary = new Cloudinary(config);
+        return cloudinary;
     }
 }
